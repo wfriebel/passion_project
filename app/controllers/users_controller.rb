b61e0fef819a62184d717423a66fe87972bcd7bb
Will Friebel <will.friebel@gmail.com>
2016-07-16 18:46:43 -0700

    Add User show page with links to each course a user has

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,24 @@
 enable :sessions
 
 post "/users" do
-	user_id = User.authenticate(params[:user])
-	if user_id
-		session[:id] = user_id
-		redirect "/users/#{user_id}"
-	else
-		@error = "Please enter a valid email and password combination"
-		erb :index
-	end
+	user = User.new(params[:user])
+	user.password = params[:password1]
+	user.save
+	session[:user_id] = user.id
+	session[:has_courses] = false
+	redirect "users/#{user.id}"
 end
 
 get "/users/new" do
 	erb :"/users/new"
 end
 
-get "/users/:id" do
-	@user = User.find(params[:id])
+get "/users/:user_id" do
+	@user = User.find(params[:user_id])
 	erb :"users/show"
+end
+
+get "/users/:user_id/courses/:course_id" do
+	session[:course_id] = params[:course_id]
+	erb :"/courses/show"
 end
--- app/helpers/formatting.rb
+++ app/helpers/formatting.rb
@@ -1,3 +1,5 @@
 helpers do
-	
+	def logged_in?
+		session[:user_id].present?
+	end
 end
